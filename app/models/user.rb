abb9be50ea07662ab80cea836d7303cf1fcf409f
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-10-12 00:44:42 -0700

    Adds the first migration of the project, the user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+
+  #user class is a model for, foremost, myself and Nikki, but additional users in the future.
+  #each user will have many movies they have seen and reviewed.
+  #each user will have a name and perhaps an icon/image.
+
+end
