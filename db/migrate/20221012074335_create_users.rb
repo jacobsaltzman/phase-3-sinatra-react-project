497ae5e99f2aeaa650e1f8318c68a6758fb16983
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-10-12 02:02:26 -0700

    Adjustments to the language

--- db/migrate/20221012074335_create_users.rb
+++ db/migrate/20221012074335_create_users.rb
@@ -3,9 +3,9 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.date :date_joined
       t.string :img_url
       t.integer :number_of_reviews
+      t.timestamps
     end
   end
 end
