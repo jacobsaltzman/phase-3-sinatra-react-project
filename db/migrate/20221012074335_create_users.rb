8b05b816ad6508a1719d8536f4b7625276566d09
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-10-12 00:59:23 -0700

    Creates the migration file and creates the table for db

--- db/migrate/20221012074335_create_users.rb
+++ db/migrate/20221012074335_create_users.rb
@@ -1,4 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.date :date_joined
+      t.string :img_url
+      t.integer :number_of_reviews
   end
 end
