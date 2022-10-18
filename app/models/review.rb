9858d677ede7a6e196f676994646e01b1007a9b5
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-10-18 00:48:50 -0700

    Adds the relationship macros for each model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
 
   belongs_to :user
-  
+  belongs_to :movies
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
   has_many :reviews
+  has_many :movies, through :reviews
   #user class is a model for, foremost, myself and Nikki, but additional users in the future.
   #each user will have many reviews and ratings of movies. 
   #each user will have a name and perhaps an icon/image and eventually a username and pw.
