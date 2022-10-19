9941450240b84a79af1e8a934edf1355989156af
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-10-18 23:01:35 -0700

    More data added for future seeding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,27 @@
 puts "🌱 Seeding movies..."
 
 
-movies_attributes = [
+movie_data = [
   {
     title: "Us",
     director: "Jordan Peele",
     type: "Mystery/Thriller",
     year: 2019,
-    image: ""
+    image: "https://upload.wikimedia.org/wikipedia/en/0/00/Us_%282019%29_theatrical_poster.png"
   },
   {
     title: "Get Out",
     director: "Jordan Peele",
     type: "Mystery/Thriller",
     year: 2017,
-    image: ""
+    image: "https://upload.wikimedia.org/wikipedia/en/a/a3/Get_Out_poster.png"
   },
   {
     title: "Psycho",
     director: "Alfred Hitchcock",
     type: "Mystery/Thriller",
     year: 1960,
-    image: ""
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Psycho_%281960%29_theatrical_poster_%28retouched%29.jpg/220px-Psycho_%281960%29_theatrical_poster_%28retouched%29.jpg"
 
   },
   {
@@ -29,96 +29,96 @@ movies_attributes = [
     director: "Robert Wiene",
     type: "Mystery/Thriller",
     year: 1919,
-    image: ""
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/2/2f/The_Cabinet_of_Dr._Caligari_poster.jpg/220px-The_Cabinet_of_Dr._Caligari_poster.jpg"
   },
   {
     title: "Alien",
     director: "Ridley Scott",
     type: "SciFi",
     year: 1979,
-    image: ""
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Alien_movie_poster.jpg/220px-Alien_movie_poster.jpg"
   },
   {
     title: "King Kong",
     director: "Merian C. Cooper",
     type: "Monster",
     year: 1933,
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/King_Kong_1933_Promotional_Image.png/280px-King_Kong_1933_Promotional_Image.png"
+  },
+  {
+    title: "Dawn of the Dead",
+    director: "Zack Snyder",
+    type: "SciFi",
+    year: "2004",
+    image: "https://upload.wikimedia.org/wikipedia/en/1/16/Dawn_of_the_Dead_2004_movie.jpg"
+  },
+  {
+    title: "The Omen",
+    director: "",
+    type: "Paranormal",
+    year: "1976",
     image: ""
   },
   {
-    title: "",
+    title: "The Grudge",
     director: "",
-    type: "",
+    type: "Paranormal",
+    year: "2004",
+    image: ""
+  },
+  {
+    title: "The Frighteners",
+    director: "",
+    type: "Comedy",
+    year: "1996",
+    image: ""
+  },
+  {
+    title: "The Blair Witch Project",
+    director: "",
+    type: "FF",
+    year: "",
+    image: ""
+  },
+  {
+    title: "Hellraiser",
+    director: "",
+    type: "Gore",
+    year: "",
+    image: ""
+  },
+  {
+    title: "Nope",
+    director: "Jordan Peele",
+    type: "SciFi",
     year: "",
     image: ""
   },
   {
     title: "",
     director: "",
-    type: "",
+    type: "Psychological",
     year: "",
     image: ""
   },
   {
-    title: "",
+    title: "Friday the 13th",
     director: "",
-    type: "",
+    type: "Slasher",
     year: "",
     image: ""
   },
   {
-    title: "",
+    title: "Godzilla",
     director: "",
-    type: "",
+    type: "Monster",
     year: "",
     image: ""
   },
   {
-    title: "",
+    title: "Shawn of the Dead",
     director: "",
-    type: "",
-    year: "",
-    image: ""
-  },
-  {
-    title: "",
-    director: "",
-    type: "",
-    year: "",
-    image: ""
-  },
-  {
-    title: "",
-    director: "",
-    type: "",
-    year: "",
-    image: ""
-  },
-  {
-    title: "",
-    director: "",
-    type: "",
-    year: "",
-    image: ""
-  },
-  {
-    title: "",
-    director: "",
-    type: "",
-    year: "",
-    image: ""
-  },
-  {
-    title: "",
-    director: "",
-    type: "",
-    year: "",
-    image: ""
-  },
-  {
-    title: "",
-    director: "",
-    type: "",
+    type: "Comedy",
     year: "",
     image: ""
   },
@@ -166,7 +166,7 @@ movies_attributes = [
   }
 ]
 
-# movies_attributes.each do |attr|
+# movie_data.each do |attr|
 #   Movie.create(attr)
 # end
 
