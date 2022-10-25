be89dab2d7c28f552e358e0613aa67a3683d8cfc
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-10-25 14:57:46 -0700

    Builds out the get and post controllers for Users and Reviews.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
   end
 
   #movies controller
+
   get "/movies" do
     movies = Movie.all 
     movies.to_json  #can do Movie.all.to_json, but keeping it separate for clarity and learning for now
@@ -23,4 +24,37 @@ class ApplicationController < Sinatra::Base
   end
 
 
+  #users controller
+
+  get "/users" do
+    users = User.all 
+    users.to_json  
+  end
+
+  get "/users/:id" do
+    user = User.find(params[:id])
+    user.to_json
+  end
+
+  post "/users" do
+    user = User.create(params)
+    user.to_json
+  end
+
+
+  #reviews controller
+  get "/reviews" do
+    reviews = Review.all 
+    reviews.to_json  
+  end
+
+  get "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.to_json
+  end
+
+  post "/reviews" do
+    review = Review.create(params)
+    review.to_json
+  end
 end
