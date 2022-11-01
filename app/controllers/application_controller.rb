caf37fd0c500c151d5dabf297e0b9083b8d94caa
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-10-31 23:27:25 -0700

    Some language changes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Boo!" }.to_json
   end
 
   #movies controller
