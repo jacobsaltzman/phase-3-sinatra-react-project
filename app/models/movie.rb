03c96de0e5678a9ce1c8985b0a14898adac67c79
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-11-15 01:53:54 -0800

    Adds two methods for finding the averages of the two user ratings.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,13 @@ class Movie < ActiveRecord::Base
   has_many :reviews #one to many
   has_many :users, through: :reviews #many to many
 
+  def avg_rating
+    self.reviews.average(:user_rating)
+  end
+
+  def avg_scary
+    self.reviews.average(:scare_scale)
+  end
+
+
 end
