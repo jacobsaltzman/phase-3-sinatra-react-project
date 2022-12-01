089509b85ad1d64adf567185b9657660cf28ee47
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-12-01 12:14:45 -0800

    Fixes the missing name in the table migration.

--- db/migrate/20221201195158_create_houses.rb
+++ db/migrate/20221201195158_create_houses.rb
@@ -1,6 +1,6 @@
 class CreateHouses < ActiveRecord::Migration[6.1]
   def change
-    create_table do |t|
+    create_table :houses do |t|
       t.string :title
       t.string :leader
       t.integer :members
